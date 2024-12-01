3b385879566e2a564ef98b9446818a9ec9a0d37d
Dmitry <dvornikov.dmitry034@gmail.com>
2024-12-01 23:02:22 +0300

    add adaptive catalog page

--- script/catalog.js
+++ script/catalog.js
@@ -11,9 +11,11 @@ let email = 'example@gmail.com'
 const headUserFunctionsCountBuy = document.querySelector('.head__userFunctions_countBuy');
 
 function addCountBuy() {
-	let getOrdersArray = localStorage.getItem(email);
-	let parseOrders = JSON.parse(getOrdersArray);
-	headUserFunctionsCountBuy.textContent = `${parseOrders.length}`;
+	if (localStorage.getItem(email)) {
+		let getOrdersArray = localStorage.getItem(email);
+		let parseOrders = JSON.parse(getOrdersArray);
+		headUserFunctionsCountBuy.textContent = `${parseOrders.length}`;
+	}
 }
 addCountBuy();
 
@@ -34,3 +36,42 @@ addBasketCatalogPage.addEventListener('click', function () {
 
 	addCountBuy();
 });
+
+// Для адаптива, меняет количество строк в теге textarea с 10 на 2
+if (window.innerWidth <= 1230) {
+	orderDescription.setAttribute('rows', "1");
+}
+
+// Кнопка закрытия формы для отправки
+const catalogFormOrderButtonClose = document.querySelector('.catalog__form_orderButtonClose');
+const catalogForm = document.querySelector('.catalog__form');
+const catalogFormDivElements = catalogForm.querySelectorAll('div');
+const catalogFormOrderButton = document.querySelector('.catalog__form_orderButton');
+
+function openCheckoutblock() {
+	catalogFormDivElements.forEach(block => {
+		block.style.display = `block`;
+	});
+	catalogFormOrderButton.style.display = `block`;
+	catalogFormOrderButtonClose.classList.remove('catalog__form_orderButtonCloseActive');
+	catalogFormOrderButtonClose.classList.add('catalog__form_orderButtonClose');
+	catalogFormOrderButtonClose.textContent = "X";
+	catalogForm.style.cssText = `border: 1px dashed $colorSelectSite;`;
+	catalogFormOrderButtonClose.removeEventListener('click', openCheckoutblock);
+	catalogFormOrderButtonClose.addEventListener('click', closingCheckoutblock);
+}
+
+function closingCheckoutblock() {
+	catalogFormDivElements.forEach(block => {
+		block.style.display = `none`;
+	});
+	catalogFormOrderButton.style.display = `none`;
+	catalogFormOrderButtonClose.classList.add('catalog__form_orderButtonCloseActive');
+	catalogFormOrderButtonClose.classList.remove('catalog__form_orderButtonClose');
+	catalogFormOrderButtonClose.textContent = "";
+	catalogForm.style.border = `none`;
+	catalogFormOrderButtonClose.removeEventListener('click', closingCheckoutblock);
+	catalogFormOrderButtonClose.addEventListener('click', openCheckoutblock);
+}
+
+catalogFormOrderButtonClose.addEventListener('click', closingCheckoutblock);
